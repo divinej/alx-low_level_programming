c9596fa8ed2142773319216235d8b95cd9266835
Divinejustice Oko <okodivinejustice@gmail.com>
2021-09-01 04:36:39 -0700

    prints a text using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
 * main - prints  a text
 *
 */
-
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
