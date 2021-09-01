c1df5eaf61ea2c8984d04a1e8c2409acb1107329
Divinejustice Oko <okodivinejustice@gmail.com>
2021-09-01 12:14:14 -0700

    prints some  text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
-* main - prints  a text
-*
-*/
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
