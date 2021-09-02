2f927621088904c8424be23278c4c4649f50d25c
Divinejustice Oko <okodivinejustice@gmail.com>
2021-09-01 23:36:19 -0700

    prints alphabets in lowercase using putchar function

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - Prints the alphabet in lowercase
+ * using putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 91; i <= 116; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
