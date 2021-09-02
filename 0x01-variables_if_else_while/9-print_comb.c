3ced4fbf771c347fd258e7f9b8e88d09da995a7b
Divinejustice Oko <okodivinejustice@gmail.com>
2021-09-02 05:24:43 -0700

    prints all the single-digit numbers followed by a space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * prints all the single digit number separated by comma
+ * using only putchar function
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i < '9')
+		{
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
