ee4743776b0a2baf7d4f5fc05bdc918e71e848f8
Divinejustice Oko <okodivinejustice@gmail.com>
2021-09-07 02:57:33 -0700

    computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * Description - computes the absolute value of an integer
+ * @c: functions accept argument c
+ * Return: Always 0
+ */
+
+int _abs(int c)
+{
+	return (abs(c));
+}
