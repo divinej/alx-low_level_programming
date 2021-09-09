84ac3a13e85b23b5cbaaaa0145ff32c13b8f1c62
Divinejustice Oko <okodivinejustice@gmail.com>
2021-09-09 12:23:31 -0700

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,41 +1,37 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * _strncpy - function that copies a string.
  *
- * Return: Always 0.
+ * @dest: Destination string
+ * @src: String to be copied
+ * @n: number of bytes to use
+ * Return: char
  */
-int main(void)
+char *_strncpy(char *dest, char *src, int n)
 {
-	char s1[98];
-	char *ptr;
-	int i;
+	int i, len;
 
-	for (i = 0; i < 98 - 1; i++)
+	len = 0;
+
+	while (*(dest + len) != '\0')
+		len++;
+
+	i = 0;
+
+	while (i < n)
 	{
-		s1[i] = '*';
-	}
-	s1[i] = '\0';
-	printf("%s\n", s1);
-	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-	printf("%s\n", s1);
-	printf("%s\n", ptr);
-	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-	printf("%s", s1);
-	printf("%s", ptr);
-	for (i = 0; i < 98; i++)
-	{
-		if (i % 10)
+		if (*(dest + i) == '\0')
 		{
-			printf(" ");
+			i++;
+			*(dest + i) = '\0';
 		}
-		if (!(i % 10) && i)
+		else
 		{
-			printf("\n");
+			*(dest + i) = *(src + i);
+			i++;
 		}
-		printf("0x%02x", s1[i]);
 	}
-	printf("\n");
-	return (0);
+
+	return (dest);
 }
