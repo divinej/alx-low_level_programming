b10a72be9b063a77025b6e13017de204dc1b7cc8
Divinejustice <okodivinejustice@gmail.com>
2021-09-20 07:18:41 -0700

    copies the string pointed to by src including null byte to the buffer pointed to by dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src, including the '\0'
+ * to the buffer pointed to by dest
+ * *@dest: pointer to the variable which stores the copied string
+ * *@stc: pointer to the string to be copied
+ * Return: the pointe to the dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		dest[i] = src[i];
+		if (src[i] == '\0')
+			break;
+		i++;
+	}
+	return (dest);
+}
